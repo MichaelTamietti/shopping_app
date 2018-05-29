d38526cf4fafef6406ef237e3fa78212b02702bc
Michael Tamietti <michael@Michaels-MacBook-Pro.local>
2018-05-29 10:36:47 -0600

    took out example lines in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 #
 # Examples:
 
-    Department.create(name: "Example", location: "Example", store_id: 1)
+    # Department.create(name: "Example", location: "Example", store_id: 1)
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
